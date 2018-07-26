34706ba690b9a2e0a4a5d23dbb219da27bf55f2e
Luis Fernando Useche <Arqsunny@users.noreply.github.com>
2018-07-26 10:14:40 -0500

    Updated Files

--- data.js
+++ data.js
@@ -258,6 +258,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
