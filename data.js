824607f684491eeb5616c9e391506a1c3c3f2242
Luis Fernando Useche <Arqsunny@users.noreply.github.com>
2018-07-26 09:18:13 -0500

    Upload Files
    Upload Files

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_location_2",
+      "name": "1_Location_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.5055783930973998,
+        "pitch": -0.10409273072037095,
+        "fov": 1.486773564447784
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2514602451983858,
+          "pitch": 0.1612517063997796,
+          "rotation": 5.497787143782138,
+          "target": "1-1_location_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2676297409238142,
+          "pitch": 0.2832317502103425,
+          "title": "Main Lobby",
+          "text": "Any additional information can be typed here"
+        }
+      ]
+    },
+    {
+      "id": "1-1_location_3",
+      "name": "1_Location_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6377972733650754,
+          "pitch": -0.046974471037106724,
+          "rotation": 0,
+          "target": "2-1_location_4"
+        },
+        {
+          "yaw": -2.054495315448971,
+          "pitch": -0.01164201394962383,
+          "rotation": 6.283185307179586,
+          "target": "0-1_location_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0436573580609316,
+          "pitch": 0.12886833178934864,
+          "title": "Main Entrance",
+          "text": "Any additional information can be typed here"
+        },
+        {
+          "yaw": -0.6378429845942684,
+          "pitch": 0.04417719523832453,
+          "title": "Panoramic Stairway",
+          "text": "Any additional information can be typed here"
+        }
+      ]
+    },
+    {
+      "id": "2-1_location_4",
+      "name": "1_Location_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6563189455426572,
+          "pitch": 0.002287023593353865,
+          "rotation": 0.7853981633974483,
+          "target": "3-1_location_5"
+        },
+        {
+          "yaw": -1.109811837936217,
+          "pitch": 0.28119097783911684,
+          "rotation": 3.9269908169872414,
+          "target": "1-1_location_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1085458183186851,
+          "pitch": 0.41160145514638913,
+          "title": "Main Lobby",
+          "text": "Any additional information can be typed here"
+        },
+        {
+          "yaw": 2.656191662472146,
+          "pitch": -0.11917717355038171,
+          "title": "Main Conference Room&nbsp; - 2nd Level",
+          "text": "Any additional information can be typed here"
+        },
+        {
+          "yaw": -2.743404169754971,
+          "pitch": -0.11568296613451956,
+          "title": "Coffee Break Area",
+          "text": "Any additional information can be typed here"
+        }
+      ]
+    },
+    {
+      "id": "3-1_location_5",
+      "name": "1_Location_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7118047926011961,
+          "pitch": 0.08866498180321436,
+          "rotation": 0.7853981633974483,
+          "target": "4-1_location_6"
+        },
+        {
+          "yaw": -1.718084587876346,
+          "pitch": 0.06083830084324937,
+          "rotation": 5.497787143782138,
+          "target": "2-1_location_4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.27973492556524,
+          "pitch": 0.47225581059264243,
+          "title": "Main Conference Room",
+          "text": "Any additional information can be typed here"
+        },
+        {
+          "yaw": 1.3197599938552749,
+          "pitch": -0.0710012387918173,
+          "title": "Whiting Wall",
+          "text": "Any additional information can be typed here"
+        },
+        {
+          "yaw": 0.7042716976119845,
+          "pitch": 0.21735591353844974,
+          "title": "Team Area",
+          "text": "Any additional information can be typed here"
+        },
+        {
+          "yaw": -1.7188352861204255,
+          "pitch": 0.19599967748768243,
+          "title": "Coffee Break Area &amp; Stairs",
+          "text": "Any additional information can be typed here"
+        }
+      ]
+    },
+    {
+      "id": "4-1_location_6",
+      "name": "1_Location_6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1410824234722643,
+          "pitch": 0.15037043742788114,
+          "rotation": 5.497787143782138,
+          "target": "3-1_location_5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "K - Project",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
